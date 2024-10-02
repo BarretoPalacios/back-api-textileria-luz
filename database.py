0f03215c2351b8c48f8dd88d101ce6bae7a77f58
David Barreto <david11barreto11@gmail.com>
2024-10-01 19:23:30 -0500

    v3 upload external db

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Cambiar por el URI de tu base de datos
+SQLALCHEMY_DATABASE_URL = "postgresql://db_textileria_luz_user:YGuCl8dNJMJkyobegrLybfbOPQzPEhRa@dpg-cru8obdumphs73ejht3g-a.oregon-postgres.render.com/db_textileria_luz"  # Cambiar por el URI de tu base de datos
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
